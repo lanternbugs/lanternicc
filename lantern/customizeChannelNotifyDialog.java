04625e2f9ed1f70f39ed026aa4487a4fe36dab80
Andrey Gorlin <silver83p@gmail.com>
2012-05-05 20:00:55 -0400

    Formatted the channel notify dialog in preparation for rewriting it.

--- lantern/customizeChannelNotifyDialog.java
+++ lantern/customizeChannelNotifyDialog.java
@@ -1,6 +1,6 @@
 package lantern;
 /*
-*  Copyright (C) 2010 Michael Ronald Adams.
+*  Copyright (C) 2012 Michael Ronald Adams, Andrey Gorlin.
 *  All rights reserved.
 *
 * This program is free software; you can redistribute it and/or
@@ -21,205 +21,167 @@ import javax.swing.*;
 import javax.swing.JDialog;
 
 
-class customizeChannelNotifyDialog extends JDialog
-{
+class customizeChannelNotifyDialog extends JDialog {
   JTextField field;
 
-JLabel messageLabel;
-JLabel addLabel;
-JLabel removeLabel;
-JButton okButton;
-JButton cancelButton;
-JButton globalButton;
-channels sharedVariables;
-int notIndex;
-customizeChannelNotifyDialog(JFrame frame, boolean mybool, channels sharedVariables1, final String name)
-{
-super(frame, mybool);
-sharedVariables=sharedVariables1;
+  JLabel messageLabel;
+  JLabel addLabel;
+  JLabel removeLabel;
+  JButton okButton;
+  JButton cancelButton;
+  JButton globalButton;
+  channels sharedVariables;
+  int notIndex;
 
-String hisChannels=getChannels(name);
-if(hisChannels == null)
-hisChannels = " null";
-setTitle("Set Channel Notify for " + name + hisChannels );
-setSize(500,300);
-setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+  customizeChannelNotifyDialog(JFrame frame, boolean mybool,
+                               channels sharedVariables1, final String name) {
+    super(frame, mybool);
+    sharedVariables = sharedVariables1;
 
+    String hisChannels = getChannels(name);
+    if (hisChannels == null)
+      hisChannels = " null";
+    setTitle("Set Channel Notify for " + name + hisChannels);
+    setSize(500,300);
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-okButton = new JButton("Ok");
-cancelButton = new JButton("Cancel");
-addLabel = new JLabel("Add channel to channel notify " + name + " or remove " + name + " from notify");
-field= new JTextField(3);
-JPanel panel = new JPanel();
-notIndex=isOnGlobalNotify(name);
-String text;
-if(notIndex==-1)
-text = "Connect Notify " + name;
-else
-text= "Un-connect Notify " + name;
-globalButton = new JButton(text);
+    okButton = new JButton("Ok");
+    cancelButton = new JButton("Cancel");
+    addLabel = new JLabel("Add channel to channel notify " + name +
+                          " or remove " + name + " from notify");
+    field = new JTextField(3);
+    JPanel panel = new JPanel();
+    notIndex = isOnGlobalNotify(name);
+    String text;
+    if (notIndex == -1)
+      text = "Connect Notify " + name;
+    else
+      text = "Un-connect Notify " + name;
+    globalButton = new JButton(text);
 
-panel.add(field, BorderLayout.SOUTH);
-panel.add(addLabel, BorderLayout.NORTH);
-panel.add(okButton, BorderLayout.CENTER);
-panel.add(cancelButton, BorderLayout.CENTER);
-panel.add(globalButton,  BorderLayout.CENTER);
-add(panel);
+    panel.add(field, BorderLayout.SOUTH);
+    panel.add(addLabel, BorderLayout.NORTH);
+    panel.add(okButton, BorderLayout.CENTER);
+    panel.add(cancelButton, BorderLayout.CENTER);
+    panel.add(globalButton,  BorderLayout.CENTER);
+    add(panel);
 
+    globalButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+          //String mytext= field.getText();
+          try {
+            if (notIndex == -1) {
+              lanternNotifyClass ln = new lanternNotifyClass();
+              ln.name = name;
+              sharedVariables.lanternNotifyList.add(ln);
+            } else
+              sharedVariables.lanternNotifyList.remove(notIndex);
+            write2();
+            dispose();
 
-globalButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event)
-				{
-                                  //String mytext= field.getText();
-				 try
-			 	{
-                                        if(notIndex==-1)
-                                        {
-                                         lanternNotifyClass ln = new lanternNotifyClass();
-                                         ln.name=name;
-                                          sharedVariables.lanternNotifyList.add(ln);
-                                        }
-                                        else
-                                         sharedVariables.lanternNotifyList.remove(notIndex);
-                                         write2();
-                                         dispose();
-                                  }
-                                  catch(Exception dui){dispose();}
+          } catch (Exception dui) {
+            dispose();
+          }
+        }// end action performed
+      });
 
-                                  }// end action performed
-});
-okButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event)
-				{
-                                  //String mytext= field.getText();
-				 try
-			 	{
-				String text = field.getText().trim();
-				int number = Integer.parseInt(text);
+    okButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+          //String mytext= field.getText();
+          try {
+            String text = field.getText().trim();
+            int number = Integer.parseInt(text);
 
-				if(number <400 && number >=0)
-				{
+            if (number < 400 && number >= 0) {
+              boolean haveChannel = false;
+              for (int z=0; z<sharedVariables.channelNotifyList.size(); z++)
+                if (sharedVariables.channelNotifyList.get(z).channel.equals(text)) {
+                  boolean found = false;
+                  for (int b=0; b<sharedVariables.channelNotifyList.get(z).nameList.size(); b++)
+                    if (sharedVariables.channelNotifyList.get(z).nameList.get(b).toLowerCase().equals(name.toLowerCase())) {
+                      sharedVariables.channelNotifyList.get(z).nameList.remove(b);
+                      found = true;
+                    }
 
+                  if (found == false) {// we have channel but he is not on list so we add him
+                    sharedVariables.channelNotifyList.get(z).nameList.add(name);
+                  }
 
+                  haveChannel = true;
+                }
 
-					boolean haveChannel=false;
-						for(int z=0; z<sharedVariables.channelNotifyList.size(); z++)
-						if(sharedVariables.channelNotifyList.get(z).channel.equals(text))
-						{
-							boolean found=false;
-							for(int b=0; b<sharedVariables.channelNotifyList.get(z).nameList.size(); b++)
-							if(sharedVariables.channelNotifyList.get(z).nameList.get(b).toLowerCase().equals(name.toLowerCase()))
-							{
-								sharedVariables.channelNotifyList.get(z).nameList.remove(b);
-								found=true;
-							}
+              if (haveChannel == false) {
+                channelNotifyClass temp = new channelNotifyClass();
+                temp.channel = text;
+                temp.nameList.add(name);
+                sharedVariables.channelNotifyList.add(temp);
+              }
+              write();
+            }// if valid number
+            dispose();
 
-							if(found==false)// we have channel but he is not on list so we add him
-							{
-								sharedVariables.channelNotifyList.get(z).nameList.add(name);
-							}
+          } catch (Exception dummy) {
+            dispose();
+          }
+        }// end event
+      });
 
-							haveChannel=true;
+    cancelButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+          //String mytext= field.getText();
+          try {
+            dispose();
+          } catch (Exception dummy) {}
+        }// end event
+      });
+  }// end constructor
 
-						}
+  String getChannels(String name) {
+    String mess=" ";
+    String channel="";
+    for (int z=0; z<sharedVariables.channelNotifyList.size(); z++)
+      if (sharedVariables.channelNotifyList.get(z).nameList.size() > 0) {
+        channel = sharedVariables.channelNotifyList.get(z).channel;
+        for (int x=0; x<sharedVariables.channelNotifyList.get(z).nameList.size(); x++)
+          if (sharedVariables.channelNotifyList.get(z).nameList.get(x).toLowerCase().equals(name.toLowerCase()))
+            mess += " " + channel;
+      }
+    return mess;
+  }
 
-						if(haveChannel==false)
-						{
-							channelNotifyClass temp = new channelNotifyClass();
-							temp.channel=text;
-							temp.nameList.add(name);
-							sharedVariables.channelNotifyList.add(temp);
-						}
-						write();
+  void write() { // channel notify
+    FileWrite writer = new FileWrite();
+    String mess = "\r\n";
+    for (int z=0; z<sharedVariables.channelNotifyList.size(); z++)
+      if (sharedVariables.channelNotifyList.get(z).nameList.size() > 0) {
+        mess +="#" + sharedVariables.channelNotifyList.get(z).channel + "\r\n";
+        for (int x=0; x < sharedVariables.channelNotifyList.get(z).nameList.size(); x++)
+          mess += sharedVariables.channelNotifyList.get(z).nameList.get(x) + "\r\n";
+      }
 
+    writer.write(mess, "lantern_channel_notify.txt");
+  }
 
+  void write2() { // global notify
+    FileWrite writer = new FileWrite();
+    String mess = "\r\n";
+    for (int z=0; z<sharedVariables.lanternNotifyList.size(); z++) {
+      String name=sharedVariables.lanternNotifyList.get(z).name;
+      if (sharedVariables.lanternNotifyList.get(z).sound == true)
+        name = name + " 1\r\n";
+      else
+        name = name+ " 0\r\n";
+      mess+=name;
+    }// end for
 
-			}// if valid number
-				dispose();
-				}
-				catch(Exception dummy){dispose();}
+    writer.write(mess, "lantern_global_notify.txt");
+  }
 
-			}// end event
+  int isOnGlobalNotify(String name) {
+    for (int z=0; z<sharedVariables.lanternNotifyList.size(); z++)
+      if (sharedVariables.lanternNotifyList.get(z).name.toLowerCase().equals(name.toLowerCase()))
+        return z;
 
-		});
-
-cancelButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event)
-				{
-                                  //String mytext= field.getText();
-				 try
-			 	{
-					dispose();
-				}
-				catch(Exception dummy){}
-
-			}// end event
-
-		});
-
-
-
-}// end constructor
-
-String getChannels(String name)
-{
-  
-String mess=" ";
-String channel="";
-for(int z=0; z<sharedVariables.channelNotifyList.size(); z++)
-if(sharedVariables.channelNotifyList.get(z).nameList.size()>0)
-{
-channel=sharedVariables.channelNotifyList.get(z).channel ;
-for(int x=0; x < sharedVariables.channelNotifyList.get(z).nameList.size(); x++)
- if(sharedVariables.channelNotifyList.get(z).nameList.get(x).toLowerCase().equals(name.toLowerCase()))
-mess+= " " + channel;
-}
-return mess;
-}
-
-
-void write()  // channel notify
-{
-
-FileWrite writer = new FileWrite();
-String mess="\r\n";
-for(int z=0; z<sharedVariables.channelNotifyList.size(); z++)
-if(sharedVariables.channelNotifyList.get(z).nameList.size()>0)
-{
-mess+="#" + sharedVariables.channelNotifyList.get(z).channel + "\r\n";
-for(int x=0; x < sharedVariables.channelNotifyList.get(z).nameList.size(); x++)
-mess+=sharedVariables.channelNotifyList.get(z).nameList.get(x) + "\r\n";
-}
-
-writer.write(mess, "lantern_channel_notify.txt");
-}
-
-void write2()  // global notify
-{
-
-FileWrite writer = new FileWrite();
-String mess="\r\n";
-for(int z=0; z<sharedVariables.lanternNotifyList.size(); z++)
-{ String name=sharedVariables.lanternNotifyList.get(z).name;
-if(sharedVariables.lanternNotifyList.get(z).sound == true)
-name=name + " 1\r\n";
-else
-name=name+ " 0\r\n";
-mess+=name;
-}// end for
-
-
-writer.write(mess, "lantern_global_notify.txt");
-}
-
-int isOnGlobalNotify(String name)
-{
-
-for(int z=0; z<sharedVariables.lanternNotifyList.size(); z++)
-if(sharedVariables.lanternNotifyList.get(z).name.toLowerCase().equals(name.toLowerCase()))
-return z;
-
-return -1;
-}
-
+    return -1;
+  }
 }// end class
