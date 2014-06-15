31df54cf00f23e2c4fb4795dd9498a0da314059f
U-ADAMS-PC2\adams <adams@adams-PC2.(none)>
2014-06-15 01:21:33 -0700

    changes to checkers circles (inner)

--- lantern/gameboardPanel.java
+++ lantern/gameboardPanel.java
@@ -413,10 +413,15 @@ the slider is on. otherwise it draws the curernt in play board*/
                   g.setColor(Color.RED);
                   g.fillOval( a + xOff  ,  b + yOff , squarex - (2*xOff), squarey - (2*yOff));
                    g.setColor(Color.BLACK);
+                 /*
+                 circle indented in a bit
                   drawCheckersCircles(g, a + xOff * 2 , b + yOff * 2 ,  squarex - (xOff * 4), squarey - (yOff * 4) );
                   drawCheckersCircles(g, a + xOff * 2  + 1, b + yOff * 2  + 1,  squarex - (xOff * 4) - 2, squarey - (yOff * 4) - 2 );
                   drawCheckersCircles(g, a + xOff * 2  + 2, b + yOff * 2  + 2,  squarex - (xOff * 4) - 4, squarey - (yOff * 4) - 4 );
-                //  drawCheckersCircles(g, a + xOff * 4, b + yOff * 4,  squarex - (xOff * 8), squarey - (yOff * 8) );
+               */
+                  drawCheckersCircles(g, a + xOff , b + yOff ,  squarex - (xOff * 2), squarey - (yOff * 2) );
+                  drawCheckersCircles(g, a + xOff  + 1, b + yOff  + 1,  squarex - (xOff * 2) - 2, squarey - (yOff * 2) - 2 );
+                  //drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
 
          }
          else if(piece == 7)
@@ -424,11 +429,16 @@ the slider is on. otherwise it draws the curernt in play board*/
                   g.setColor(Color.BLACK);
                  g.fillOval( a + xOff  ,  b + yOff , squarex - (2*xOff), squarey - (2*yOff));
                   g.setColor(Color.WHITE);
-                  drawCheckersCircles(g, a + xOff * 2 , b + yOff * 2 ,  squarex - (xOff * 4), squarey - (yOff * 4) );
+                 /* 
+                 circle indented in a bit
+                 drawCheckersCircles(g, a + xOff * 2 , b + yOff * 2 ,  squarex - (xOff * 4), squarey - (yOff * 4) );
                   drawCheckersCircles(g, a + xOff * 2  + 1, b + yOff * 2  + 1,  squarex - (xOff * 4) - 2, squarey - (yOff * 4) - 2 );
                   drawCheckersCircles(g, a + xOff * 2  + 2, b + yOff * 2  + 2,  squarex - (xOff * 4) - 4, squarey - (yOff * 4) - 4 );
-
+                   */
                  // drawCheckersCircles(g, a + xOff * 4, b + yOff * 4,  squarex - (xOff * 8), squarey - (yOff * 8) );
+                  drawCheckersCircles(g, a + xOff , b + yOff ,  squarex - (xOff * 2), squarey - (yOff * 2) );
+                  drawCheckersCircles(g, a + xOff  + 1, b + yOff  + 1,  squarex - (xOff * 2) - 2, squarey - (yOff * 2) - 2 );
+                  //drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
 
           }
           if(piece == 6)
@@ -443,7 +453,7 @@ the slider is on. otherwise it draws the curernt in play board*/
                        g.setColor(Color.BLACK);
                   drawCheckersCircles(g, a + xOff , b + yOff ,  squarex - (xOff * 2), squarey - (yOff * 2) );
                   drawCheckersCircles(g, a + xOff  + 1, b + yOff  + 1,  squarex - (xOff * 2) - 2, squarey - (yOff * 2) - 2 );
-                  drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
+                 // drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
 
 
         }
@@ -460,7 +470,7 @@ the slider is on. otherwise it draws the curernt in play board*/
                         g.setColor(Color.WHITE);
                   drawCheckersCircles(g, a + xOff , b + yOff ,  squarex - (xOff * 2), squarey - (yOff * 2) );
                   drawCheckersCircles(g, a + xOff  + 1, b + yOff  + 1,  squarex - (xOff * 2) - 2, squarey - (yOff * 2) - 2 );
-                  drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
+                 // drawCheckersCircles(g, a + xOff  + 2, b + yOff  + 2,  squarex - (xOff * 2) - 4, squarey - (yOff * 2) - 4 );
 
 
 
