5482c5e3dd8a24a2c66c0d3e1aa54ff019389cb8
U-ADAMS-PC2\adams <adams@adams-PC2.(none)>
2017-02-22 00:38:37 -0800

    upped version as test to 5.67a

--- lantern/channels.java
+++ lantern/channels.java
@@ -358,7 +358,7 @@ JTextPane engineField = new JTextPane();
 channels()
 {
 myServer = "ICC";
-version = "v5.67";
+version = "v5.67a";
 debug = false;
 newUserMessage="Welcome to Lantern Chess! Look at Help in the Menu for some questions and support at lanternbugs at gmail.\n";
 engineDirectory = null;
