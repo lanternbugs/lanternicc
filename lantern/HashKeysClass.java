82a6b6be1387974a4839ce9040d2cf37d99fe29d
lanternbugs <michaeladams1@Michaels-Mac-mini-2.local>
2017-03-03 00:59:33 -0800

    added liscence to hashkeys.java and deleted one more file that is not part of program but extraneous

--- lantern/HashKeysClass.java
+++ lantern/HashKeysClass.java
@@ -1,4 +1,18 @@
 package lantern;
+/*
+ *  Copyright (C) 2010 Michael Ronald Adams.
+ *  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  This code is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ */
 import java.math.BigInteger;
 class HashKeysClass {
 static BigInteger [][] hashboard = new BigInteger[64][25];
