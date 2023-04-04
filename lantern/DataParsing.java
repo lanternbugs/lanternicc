96cd0838b2ee4023b2790d5f082afa93ebfd597b
Michael Adams <lanternbugs@gmail.com>
2023-04-04 16:23:42 -0700

    fixes for variant parsing on fics

--- lantern/DataParsing.java
+++ lantern/DataParsing.java
@@ -2986,11 +2986,13 @@ String myaway=sharedVariables.lanternAways.get(randomIndex);
         temp.arg3 = myGameStruct.getBlackName();
         temp.arg4 = "0";
         if(wild.equals("atomic")) {
-            temp.arg4 = "22";
+            temp.arg4 = "27";
         } else if(wild.equals("losers")) {
             temp.arg4 = "17";
         } else if(wild.equals("suicide")) {
             temp.arg4 = "26";
+        } else if(wild.equals("wild/fr")) {
+            temp.arg4 = "22";
         }
         temp.arg5 = !temp.arg4.equals("0") ? wild : "blitz";
         temp.arg6 = "0";
